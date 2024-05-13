4da43c2c7c3cdb5e70f64c0fe55a8efa565be7a2
Baha <kaisarbek2003@gmail.com>
2024-05-13 11:12:24 +0500

    updated token

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -2,14 +2,18 @@ const userService = require('../service/user-service')
 class UserControllers {
     async registration(req, res, next) {
         try {
-            const {email, password} = req.body
-            const userData = await userService.registration(email, password)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
-            return res.json(userData)
+            const { email, password } = req.body;
+            if (!email || !password) {
+                return res.status(400).json({ error: 'Email and password are required' });
+            }
+            const tokensAndUser = await userService.registration(email, password);
+            res.cookie('refreshToken', tokensAndUser.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+            return res.json(tokensAndUser);
         } catch (e) {
-            console.log(e)
+            console.log(e);
+            return res.status(500).json({ error: 'Internal server error' });
         }
-}
+    }
 
 async login(req, res, next) {
     try {
--- server/models/token-model.js
+++ server/models/token-model.js
@@ -2,7 +2,7 @@ const {Schema, model} = require('mongoose');
 
 const TokenSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User'},
-    refeshToken: { type: String, required: true },
+    refeshToken: { type: String },
 
 });
 
